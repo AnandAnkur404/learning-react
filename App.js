ff8f18822c4c4b0b06f5ac88b3850e9322e1b6b7
Ankur <ankurofficial0005@gmail.com>
2024-02-21 03:37:58 +0530

    Food Ordering app

--- App.js
+++ App.js
@@ -1,28 +1,105 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello world form react"
-// );
-
-//  root.render(heading); 
-
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Heading h1 from ankur"),
-    React.createElement("h2", {}, "Heading h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Heading h1"),
-    React.createElement("h2", {}, "Heading h2"),
-  ]),
-);
+/*
+Header
+  -logo
+  -Nav Items
+Body
+  -search
+  -RestaurantsContainer
+    -RestaurantsCards
+      image
+      name of res
+      cousin
+      Star rating
+      price 
+      delivery time
+Footer
+  -Copyright
+  -Links
+  -Address of the company
+  -contacts
+
+*/
+
+// Header component
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantsCards = (props) => {
+  // console.log(props)
+  const { resName , cuisine } = props;
+  return (
+    <div
+      className="res-cards"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://d1rgpf387mknul.cloudfront.net/products/PLP/ml/2x_ml_20220221050422896040_426x234jpg"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>4.3 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* restaurants cards */}
+        <RestaurantsCards
+          resName="Panjabi parathan"
+          cuisine="Paratha, South cousin, Asia"
+        />
+        <RestaurantsCards
+          resName="mumbai Nan"
+          cuisine="Nan, South cousin, Asia"
+        />
+        
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header
+      Body
+      Footer */}
+
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-// heading will return object.
-// render method will take this heading object and create h1 tag and put it inside root element
+
+root.render(<AppLayout />);
